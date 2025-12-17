728323136e40f3cf69f02c373cee9a8e06f353b2
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-17 11:35:04 +0530

    [Arya Mishra] added  SimpleInterest.java

---
+++ gcr-codebase/programming-prerequisites/RectanglePerimeter.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RectanglePerimeter{
+    public static void main(String[] args){
+	   Scanner sc = new Scanner(System.in);
+	   System.out.println("Enter length: ");
+	   double length = sc.nextDouble();
+	   
+	   System.out.println("Enter width: ");
+	   double width = sc.nextDouble();
+	   
+       double perimeter = 2*(length + width);
+       System.out.println("Perimeter of rectangle is : " +perimeter);	   
+	}
+}
