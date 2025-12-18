f47a5a815c27bf50fe7a6d2b2473daca80147a65
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-18 06:30:35 +0530

    [Arya Mishra] : added CalculateAge.java

---
+++ gcr-codebase/programming-elements/Level 1 Programs/CalculateAge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CalculateAge{
+    public static void main(String[] args){
+		
+	    int harrybirthYear = 2000;
+		int currentBirthYear = 2024;
+		
+		// Calculate Age of Harry
+		int ageOfHarry = currentBirthYear - harrybirthYear;
+		
+		System.out.println("Harry's age in 2024 is " +ageOfHarry);
+	}
+}
+		
