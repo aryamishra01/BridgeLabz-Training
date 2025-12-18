69218299df56f4bfc391d73423963876d0a46e03
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-18 15:01:03 +0530

    [Arya Mishra] : added AveragePercentMarks.java

---
+++ gcr-codebase/programming-elements/Level 1 Programs/AveragePercentMarks.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class AveragePercentMarks{
+    public static void main(String[] args){
+		
+		// Declaring and initializing marks
+	    double marksInMath = 94;
+        double marksInPhysics = 95;
+        double marksInChemistry = 96;
+        
+		//Calculate Average marks 
+		double avgMarks = (marksInMath + marksInPhysics+ marksInChemistry)/3;
+		
+		System.out.println("Sam's average mark in PCM is " +avgMarks);
+	}
+}
