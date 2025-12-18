29f8eb7ecaa21b73eece776541043e7cce0c68db
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-18 06:37:02 +0530

    [Arya Mishra] : added HeightConversion.java

---
+++ gcr-codebase/programming-elements/Level 1 Programs/HeightConversion.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class HeightConversion{
+    public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter height in cm: ");
+	    double heightInCm = input.nextDouble();
+		
+		//Conversion from Cm to inches and inches to feet
+		double totalInches = heightInCm / 2.54;
+		int heightInFeet = (int) (totalInches / 12);
+        double heightInInches = (int)totalInches % 12;
+		
+		System.out.println("Your Height in cm is " +heightInCm +" while in feet is " +heightInFeet +" and inches " +heightInInches);
+        
+		input.close();
+	}
+}
