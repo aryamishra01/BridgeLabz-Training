3968b36c44b5f70ce5cef74713e38ad856e76f5c
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-18 15:20:08 +0530

    [Arya Mishra] : added KilometerToMiles.java

---
+++ gcr-codebase/programming-elements/Level 1 Programs/KilometerToMiles.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class KilometerToMiles{
+    public static void main(String[] args){
+		
+	    double distance = 10.8;
+		
+		//Calculate Distance in Miles
+		double distanceInMiles = distance * 1.6;
+		
+		System.out.println("The distance "+distance +" in miles is " +distanceInMiles);
+	}
+}
+	
