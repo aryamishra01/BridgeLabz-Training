66053f4ee450d8fc6a8ceee12b55dac1b8dc9b99
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-28 01:57:25 +0530

    [Arya Mishra] : added FindFirstOccurence

---
+++ leet-code-codebase/String-Practice/FindFirstOccurrence.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class FindFirstOccurrence {
+
+    public static int strStr(String haystack, String needle) {
+
+        if (needle.length() == 0)
+            return 0;
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+
+            int j = 0;
+            while (j < needle.length() &&
+                   haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+
+            if (j == needle.length()) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter haystack string: ");
+        String haystack = sc.nextLine();
+
+        System.out.print("Enter needle string: ");
+        String needle = sc.nextLine();
+
+        int result = strStr(haystack, needle);
+
+        System.out.println("Output: " + result);
+
+        sc.close();
+    }
+}
