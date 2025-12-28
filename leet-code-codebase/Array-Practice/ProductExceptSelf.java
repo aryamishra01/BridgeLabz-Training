700c4979485275f648a621934e9bdf3d7d022627
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-28 23:13:10 +0530

    [Arya Mishra] : added ProductExceptSelf

---
+++ leet-code-codebase/Array-Practice/ProductExceptSelf.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // Step 1: prefix products
+        result[0] = 1;
+        for (int i = 1; i < n; i++) {
+            result[i] = result[i - 1] * nums[i - 1];
+        }
+
+        // Step 2: suffix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * suffix;
+            suffix *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int[] ans = productExceptSelf(nums);
+
+        System.out.print("Output: ");
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
