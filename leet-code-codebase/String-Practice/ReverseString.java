88be20351a420d774927803b7b92c821ebfd15d3
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-28 23:43:05 +0530

    [Arya Mishra] : added ReverseString

---
+++ leet-code-codebase/String-Practice/ReverseString.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ReverseString {
+
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String input = sc.nextLine();
+
+        char[] s = input.toCharArray();
+
+        reverseString(s);
+
+        System.out.print("Reversed string: ");
+        for (char c : s) {
+            System.out.print(c);
+        }
+    }
+}
