3922d20fb79f09e7699f8c447ae949447e89aaf9
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-28 23:44:16 +0530

    [Arya Mishra] : added ReverseVowels

---
+++ leet-code-codebase/String-Practice/ReverseVowels.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class ReverseVowels {
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+            || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+    public static String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && !isVowel(arr[left])) {
+                left++;
+            }
+
+            while (left < right && !isVowel(arr[right])) {
+                right--;
+            }
+
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = sc.nextLine();
+
+        System.out.println("Output: " + reverseVowels(s));
+    }
+}
