19ebdf98a5a070aab840998a319268ef213a0b59
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-31 23:25:19 +0530

    [Arya Mishra] : added School

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/selfproblems/problem1/School.java
@@ -0,0 +1,27 @@
+package com.objectmodeling.selfproblems.problem1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+
+    private String schoolName;
+    private List<Student> students;
+
+    public School(String schoolName) {
+        this.schoolName = schoolName;
+        this.students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void showStudents() {
+        System.out.println("Students in " + schoolName + ":");
+        for (Student s : students) {
+            System.out.println("- " + s.getName());
+        }
+    }
+}
+
