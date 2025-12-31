0fc40edeee692f433ebbd6b31a66b3a083885f0c
aryamishra01 <aryagetgoals12@gmail.com>
2025-12-31 23:42:14 +0530

    [Arya Mishra] : added Product

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/selfproblems/problem4/Product.java
@@ -0,0 +1,24 @@
+package com.objectmodeling.selfproblems.problem4;
+
+//Product exists independently and can be reused in many orders
+public class Product {
+
+ private int productId;
+ private String name;
+ private double price;
+
+ public Product(int productId, String name, double price) {
+     this.productId = productId;
+     this.name = name;
+     this.price = price;
+ }
+
+ public double getPrice() {
+     return price;
+ }
+
+ public String getName() {
+     return name;
+ }
+}
+
