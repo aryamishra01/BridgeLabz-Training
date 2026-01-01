365fff10bdbf871118342ab15e8029b349424253
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 16:36:21 +0530

    [Arya Mishra] : added Vehicle

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/assistedproblems/problem3/Vehicle.java
@@ -0,0 +1,20 @@
+package com.inheritance.assistedproblems.problem3;
+
+//Superclass representing common vehicle properties
+public class Vehicle {
+
+ protected int maxSpeed;
+ protected String fuelType;
+
+ public Vehicle(int maxSpeed, String fuelType) {
+     this.maxSpeed = maxSpeed;
+     this.fuelType = fuelType;
+ }
+
+ // Method to be overridden
+ public void displayInfo() {
+     System.out.println("Max Speed      : " + maxSpeed + " km/h");
+     System.out.println("Fuel Type      : " + fuelType);
+ }
+}
+
