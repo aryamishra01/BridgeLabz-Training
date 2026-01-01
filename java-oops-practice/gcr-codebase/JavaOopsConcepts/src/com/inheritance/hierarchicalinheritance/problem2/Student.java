29844545d3d1cd36726c3ff944dde19017582966
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 17:06:03 +0530

    [Arya Mishra] : added Student

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Student.java
@@ -0,0 +1,18 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Student extends Person {
+
+	    String grade;
+
+	    public Student(String name, int age, String grade) {
+	        super(name, age);
+	        this.grade = grade;
+	    }
+
+	    public void displayRole() {
+	        System.out.println("Role  : Student");
+	        System.out.println("Grade : " + grade);
+	    }
+}
+
+
