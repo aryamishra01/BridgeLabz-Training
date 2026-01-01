1f637cb7c2f823354e4bcc11931b078d369e892b
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 16:55:24 +0530

    [Arya Mishra] : added  DemonstrateMultipleInheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/multilevelinheritance/problem1/DemonstrateMultipleInheritance.java
@@ -0,0 +1,12 @@
+package com.inheritance.multilevelinheritance.problem1;
+
+public class DemonstrateMultipleInheritance {
+
+	public static void main(String[] args) {
+		
+		DeliveredOrder order = new DeliveredOrder(101,"2024-12-15",56789,"2024-12-18" );
+
+	    order.getOrderStatus();
+	}
+
+}
