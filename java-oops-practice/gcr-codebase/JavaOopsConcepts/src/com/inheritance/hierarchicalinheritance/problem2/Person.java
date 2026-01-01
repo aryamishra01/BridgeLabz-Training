066946e1493dab442454b238d2eb2c8d061711d2
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 17:05:13 +0530

    [Arya Mishra] : added Person

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem2/Person.java
@@ -0,0 +1,17 @@
+package com.inheritance.hierarchicalinheritance.problem2;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void displayBasicInfo() {
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+    }
+}
