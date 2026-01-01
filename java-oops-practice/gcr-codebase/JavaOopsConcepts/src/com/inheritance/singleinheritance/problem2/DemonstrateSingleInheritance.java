bbcd107a31becb57fdf6cfb4d55f25a84d6c348a
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 16:49:20 +0530

    [Arya Mishra] : added DemonstrateSingleInheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/singleinheritance/problem2/DemonstrateSingleInheritance.java
@@ -0,0 +1,12 @@
+package com.inheritance.singleinheritance.problem2;
+
+public class DemonstrateSingleInheritance {
+
+	public static void main(String[] args) {
+		
+		Thermostat thermostat = new Thermostat("THERMO-101","ON",24.5);
+
+	    thermostat.displayStatus();
+	}
+
+}
