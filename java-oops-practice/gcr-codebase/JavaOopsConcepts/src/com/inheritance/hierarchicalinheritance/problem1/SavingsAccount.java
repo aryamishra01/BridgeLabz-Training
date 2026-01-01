3e1499609addbc1a6e1ba4f4bd8ae291a215df8a
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-01 17:00:39 +0530

    [Arya Mishra] : added  SavingsAccount

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/hierarchicalinheritance/problem1/SavingsAccount.java
@@ -0,0 +1,16 @@
+package com.inheritance.hierarchicalinheritance.problem1;
+
+public class SavingsAccount extends BankAccount {
+
+    double interestRate;
+
+    public SavingsAccount(String accountNumber, double balance, double interestRate) {
+        super(accountNumber, balance);
+        this.interestRate = interestRate;
+    }
+
+    public void displayAccountType() {
+        System.out.println("Account Type   : Savings Account");
+        System.out.println("Interest Rate : " + interestRate + "%");
+    }
+}
