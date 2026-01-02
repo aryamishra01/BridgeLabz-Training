729d4bca3b34ac24bd134cb6c85d25e2b22dd2c5
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-02 23:58:38 +0530

    [Arya Mishra] : added Product

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem2/Product.java
@@ -0,0 +1,48 @@
+package com.encapsulation.problem2;
+
+public abstract class Product {
+
+    // Encapsulated fields
+    private int productId;
+    private String name;
+    private double price;
+
+    // Constructor
+    public Product(int productId, String name, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and setters (controlled access)
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    /*
+     * Abstract method
+     * Each product type defines its own discount logic
+     */
+    public abstract double calculateDiscount();
+}
