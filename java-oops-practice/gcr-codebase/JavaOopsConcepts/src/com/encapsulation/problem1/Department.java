a9621188fc2152b3f36bbecd788fdcbb2aa9687d
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-02 23:48:40 +0530

    [Arya Mishra] : added Department

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem1/Department.java
@@ -0,0 +1,11 @@
+package com.encapsulation.problem1;
+
+public interface Department {
+
+    // Assign a department to an employee
+    void assignDepartment(String departmentName);
+
+    // Return department details
+    String getDepartmentDetails();
+}
+
