b615b76488c21205dbab6114d76ae6fecb320f7d
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-03 00:25:26 +0530

    [Arya Mishra] : added Loanable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem4/Loanable.java
@@ -0,0 +1,10 @@
+package com.encapsulation.problem4;
+
+public interface Loanable {
+
+	// Apply for loan
+    void applyForLoan(double amount);
+
+    // Calculate loan eligibility
+    double calculateLoanEligibility();
+}
