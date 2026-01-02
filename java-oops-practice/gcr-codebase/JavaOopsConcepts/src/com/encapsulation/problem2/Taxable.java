c5fa56b72516c8cb6ebe64c93556e8d8c6d365fc
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-03 00:00:52 +0530

    [Arya Mishra] : added Taxable

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem2/Taxable.java
@@ -0,0 +1,10 @@
+package com.encapsulation.problem2;
+
+public interface Taxable {
+
+    // Method to calculate tax amount
+    double calculateTax();
+
+    // Method to return tax details
+    String getTaxDetails();
+}
