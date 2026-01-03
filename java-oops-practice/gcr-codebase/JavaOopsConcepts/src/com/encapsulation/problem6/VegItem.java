5f8ba14dab143c890e08dbab04221ffac46e4978
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-03 09:50:51 +0530

    [Arya Mishra] : added VegItem

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem6/VegItem.java
@@ -0,0 +1,23 @@
+package com.encapsulation.problem6;
+
+public class VegItem extends FoodItem implements Discountable {
+
+    public VegItem(String itemName, double price, int quantity) {
+        super(itemName, price, quantity);
+    }
+
+    @Override
+    public double calculateTotalPrice() {
+        return price * quantity;  // No extra charge
+    }
+
+    @Override
+    public double applyDiscount() {
+        return calculateTotalPrice() * 0.10; // 10% discount
+    }
+
+    @Override
+    public String getDiscountDetails() {
+        return "10% discount applied on vegetarian items.";
+    }
+}
