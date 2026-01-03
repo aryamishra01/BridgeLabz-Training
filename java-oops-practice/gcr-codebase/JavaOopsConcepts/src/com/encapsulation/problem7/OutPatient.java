4aaa66f057733b2d739da537b139234e495a89e8
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-03 11:05:54 +0530

    [Arya Mishra] : added OutPatient

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulation/problem7/OutPatient.java
@@ -0,0 +1,16 @@
+package com.encapsulation.problem7;
+
+public class OutPatient extends Patient {
+
+    private double consultationFee;
+
+    public OutPatient(int patientId, String name, int age, double consultationFee) {
+        super(patientId, name, age);
+        this.consultationFee = consultationFee;
+    }
+
+    @Override
+    public double calculateBill() {
+        return consultationFee;
+    }
+}
