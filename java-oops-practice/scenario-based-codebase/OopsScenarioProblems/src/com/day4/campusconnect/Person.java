e96e783e9ec80e650a9085402b1e298a2c748ecd
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-05 20:42:27 +0530

    [Arya Mishra] : added Person

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day4/campusconnect/Person.java
@@ -0,0 +1,20 @@
+package com.day4.campusconnect;
+
+public class Person {
+
+	protected int id;
+    protected String name;
+    protected String email;
+
+    Person(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+}
