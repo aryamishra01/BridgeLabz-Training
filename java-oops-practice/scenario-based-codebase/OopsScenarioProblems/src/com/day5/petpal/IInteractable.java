11b5c009cf473ca10841b9d473d9042f76e6fdf4
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-06 22:22:18 +0530

    [Arya Mishra] : added IInteractable

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day5/petpal/IInteractable.java
@@ -0,0 +1,14 @@
+package com.day5.petpal;
+
+//Interface that defines common pet actions
+public interface IInteractable {
+
+	 // Method to feed the pet
+	 void feed();
+	
+	 // Method to play with the pet
+	 void play();
+	
+	 // Method to make the pet sleep
+	 void sleep();
+}
