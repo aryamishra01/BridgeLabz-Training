9ce9921af5da7cc11db302c99f45d07111060430
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-08 22:15:05 +0530

    [Arya Mishra] : added Transport

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day7/tourmate/Transport.java
@@ -0,0 +1,14 @@
+package com.day7.tourmate;
+
+class Transport {
+
+    private double cost; // hidden cost
+
+    public Transport(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
