0551cc72f81a8353e27fa9ed6b85652c83e28f8f
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-08 11:34:49 +0530

    [Arya Mishra] : added ReadFileUsingFileReader

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/filereader/ReadFileUsingFileReader.java
@@ -0,0 +1,22 @@
+package com.filereader;
+
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ReadFileUsingFileReader {
+
+	public static void main(String[] args) throws IOException {
+
+        FileReader fr = new FileReader("sample.txt");
+        BufferedReader br = new BufferedReader(fr);
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+
+        br.close();
+        fr.close();
+    }
+}
