1506052be74c84e759c2cf9320ca27566ec70529
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-08 22:13:06 +0530

    [Arya Mishra] : added Hotel

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day7/tourmate/Hotel.java
@@ -0,0 +1,15 @@
+package com.day7.tourmate;
+
+class Hotel {
+
+    private double cost; // encapsulated
+
+    public Hotel(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
