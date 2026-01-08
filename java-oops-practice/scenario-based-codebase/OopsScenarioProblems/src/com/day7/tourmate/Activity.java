11e3645a676921729a0068c633dd08df7c1afae4
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-08 22:06:24 +0530

    [Arya Mishra] : added Activity

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day7/tourmate/Activity.java
@@ -0,0 +1,15 @@
+package com.day7.tourmate;
+
+class Activity {
+
+    private double cost; // hidden internal cost
+
+    public Activity(double cost) {
+        this.cost = cost;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
