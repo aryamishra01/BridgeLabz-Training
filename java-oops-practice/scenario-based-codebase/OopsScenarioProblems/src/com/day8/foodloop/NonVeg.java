5dcee8da0e6b6623797454c5ddff9c1aa06eb697
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-09 16:56:19 +0530

    [Arya Mishra] : added NonVeg

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day8/foodloop/NonVeg.java
@@ -0,0 +1,9 @@
+package com.day8.foodloop;
+
+public class NonVeg extends FoodItem {
+
+    public NonVeg(String name, double price, int stock) {
+        super(name, "Non-Veg", price, stock);
+    }
+}
+
