0c90c648c8f8f1b6af3a38658003f824e91203f3
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-09 16:59:18 +0530

    [Arya Mishra] : added User

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day8/skillforge/User.java
@@ -0,0 +1,15 @@
+package com.day8.skillforge;
+
+//Base User
+class User {
+	 protected String name;
+	
+	 public User(String name) {
+	     this.name = name;
+	 }
+	
+	 public String getName() {
+	     return name;
+	 }
+}
+
