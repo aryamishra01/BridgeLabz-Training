1ce2437a8c0517702ef5e0c53a24d3c45db9d9a6
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-13 15:59:17 +0530

    [Arya Mishra] : added StageNode

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day3/parceltracker/StageNode.java
@@ -0,0 +1,13 @@
+package com.day3.parceltracker;
+
+public class StageNode {
+
+    String stageName;
+    StageNode next;
+
+    public StageNode(String stageName) {
+        this.stageName = stageName;
+        this.next = null;
+    }
+}
+
