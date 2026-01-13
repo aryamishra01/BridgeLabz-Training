74a7ee7caf3661f8a4748b6a950e6fd6841c1a0f
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-13 11:35:36 +0530

    [Arya Mishra] : added Book

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day2/bookshelf/Book.java
@@ -0,0 +1,17 @@
+package com.day2.bookshelf;
+
+class Book {
+    String title;
+    String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author;
+    }
+}
+
