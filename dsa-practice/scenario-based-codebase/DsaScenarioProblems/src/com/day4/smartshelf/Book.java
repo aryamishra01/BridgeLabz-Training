df385960c19f91a6f0254bd57a2f33da60812f4c
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-16 10:47:53 +0530

    [Arya Mishra] : added Book

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day4/smartshelf/Book.java
@@ -0,0 +1,11 @@
+package com.day4.smartshelf;
+
+public class Book {
+
+	String title;
+
+    // Constructor to initialize book title
+    Book(String title) {
+        this.title = title;
+    }
+}
