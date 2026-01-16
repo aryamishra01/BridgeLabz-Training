abc5eb2a7767cb4075adac187e12e630c81df3ab
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-16 10:53:18 +0530

    [Arya Mishra] : added Student

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day4/examcell/Student.java
@@ -0,0 +1,13 @@
+package com.day4.examcell;
+
+public class Student {
+
+	int id;
+    int score;
+
+    // Constructor to initialize student details
+    Student(int id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+}
