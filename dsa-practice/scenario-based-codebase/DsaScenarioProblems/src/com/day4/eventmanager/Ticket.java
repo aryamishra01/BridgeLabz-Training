461198d2f8205af3d85d3e4593d33e29ba1eb10d
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-16 10:51:13 +0530

    [Arya Mishra] : added Ticket

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day4/eventmanager/Ticket.java
@@ -0,0 +1,13 @@
+package com.day4.eventmanager;
+
+public class Ticket {
+
+	String eventName;
+    double price;
+
+    // Constructor to initialize ticket details
+    Ticket(String eventName, double price) {
+        this.eventName = eventName;
+        this.price = price;
+    }
+}
