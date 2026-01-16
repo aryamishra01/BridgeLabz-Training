a6bb0bcf280f76207d6725e7c5f34d29daac74ee
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-16 10:49:38 +0530

    [Arya Mishra] : added Sale

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day4/zipzipmart/Sale.java
@@ -0,0 +1,13 @@
+package com.day4.zipzipmart;
+
+public class Sale {
+
+	String date;
+    double amount;
+
+    // Constructor to initialize sale details
+    Sale(String date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+}
