90d0f121112ccf16f582c40ab6276594f4582834
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-18 13:40:06 +0530

    [Arya Mishra] : added FleetManager

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day6/fleetmanager/FleetManager.java
@@ -0,0 +1,58 @@
+package com.day6.fleetmanager;
+
+public class FleetManager {
+
+    public static void mergeSort(Vehicle[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Merge two sorted halves
+    private static void merge(Vehicle[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        Vehicle[] L = new Vehicle[n1];
+        Vehicle[] R = new Vehicle[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i].mileage <= R[j].mileage) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
