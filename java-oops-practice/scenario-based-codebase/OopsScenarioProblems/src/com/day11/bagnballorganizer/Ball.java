df3e3e5bbbdeae564977ec195a779c15a418a213
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-19 10:18:30 +0530

    [Arya Mishra] : added Ball

---
+++ java-oops-practice/scenario-based-codebase/OopsScenarioProblems/src/com/day11/bagnballorganizer/Ball.java
@@ -0,0 +1,24 @@
+package com.day11.bagnballorganizer;
+
+public class Ball implements Storable {
+
+    private String id;
+    private String color;
+    private String size;
+
+    public Ball(String id, String color, String size) {
+        this.id = id;
+        this.color = color;
+        this.size = size;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDetails() {
+        return "Ball ID: " + id + " Color: " + color + " Size: " + size;
+    }
+}
+
+
