e77854112d1c7a55262420a9cf6d303eb6f67e2a
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-20 14:25:25 +0530

    [Arya Mishra] : added Product

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day7/flashdealz/Product.java
@@ -0,0 +1,14 @@
+package com.day7.flashdealz;
+
+public class Product {
+
+	String name;
+    double price;
+    double discount;
+
+    Product(String name, double price, double discount) {
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+    }
+}
