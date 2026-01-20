d96b2d2994880c0c3c8307d0cfb51da5821df919
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-20 14:33:49 +0530

    [Arya Mishra] : added User

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day7/fitnesstracker/User.java
@@ -0,0 +1,12 @@
+package com.day7.fitnesstracker;
+
+public class User {
+
+	String name;
+    int steps;
+
+    User(String name, int steps) {
+        this.name = name;
+        this.steps = steps;
+    }
+}
