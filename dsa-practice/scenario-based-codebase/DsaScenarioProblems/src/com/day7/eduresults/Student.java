1c7d4dde52034ee63507177067e601c7045ba705
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-20 14:22:01 +0530

    [Arya Mishra] : added Student

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day7/eduresults/Student.java
@@ -0,0 +1,13 @@
+package com.day7.eduresults;
+
+public class Student {
+    String name;
+    String district;
+    int marks;
+
+    Student(String name, String district, int marks) {
+        this.name = name;
+        this.district = district;
+        this.marks = marks;
+    }
+}
