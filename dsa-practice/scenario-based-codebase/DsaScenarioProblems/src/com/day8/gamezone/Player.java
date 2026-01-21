429096b88aebd35900772877ae2e2c1e75da7a15
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-21 15:42:59 +0530

    [Arya Mishra] : added Player

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day8/gamezone/Player.java
@@ -0,0 +1,13 @@
+package com.day8.gamezone;
+
+public class Player {
+
+    String name;
+    int score;
+
+    public Player(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
+
