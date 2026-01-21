96ac706e3d2a9c20b45490fa52eee8f6c00a3962
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-21 16:09:48 +0530

    [Arya Mishra] : added Medicine

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day9/medwarehouse/Medicine.java
@@ -0,0 +1,17 @@
+package com.day9.medwarehouse;
+
+public class Medicine {
+
+	String name;
+    int day;
+    int month;
+    int year;
+
+    public Medicine(String name, int day, int month, int year) {
+        this.name = name;
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+
