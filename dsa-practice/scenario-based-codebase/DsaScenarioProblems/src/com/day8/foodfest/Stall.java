be8761bd3d37010d5ec387e8bb1b22c187386fad
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-21 15:40:51 +0530

    [Arya Mishra] : added Stall

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day8/foodfest/Stall.java
@@ -0,0 +1,12 @@
+package com.day8.foodfest;
+
+public class Stall {
+
+	String name;
+    int footfall;
+
+    public Stall(String name, int footfall) {
+        this.name = name;
+        this.footfall = footfall;
+    }
+}
