d0ecc3727043e76b479f5d0327df311c5496e26c
aryamishra01 <aryagetgoals12@gmail.com>
2026-01-21 16:01:07 +0530

    [Arya Mishra] : added Theatre

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioProblems/src/com/day8/movietime/Theatre.java
@@ -0,0 +1,12 @@
+package com.day8.movietime;
+
+public class Theatre {
+
+	String movie;
+	String time;
+	
+	public Theatre(String movie, String time) {
+		this.movie = movie;
+		this.time = time;
+	}
+}
