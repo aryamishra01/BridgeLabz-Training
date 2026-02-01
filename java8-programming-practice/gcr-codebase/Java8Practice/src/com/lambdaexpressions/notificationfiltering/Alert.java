22d1b86c513f99032b0ef2778a8fe058d2849ea4
aryamishra01 <aryagetgoals12@gmail.com>
2026-02-01 21:14:24 +0530

    [Arya Mishra] : added Alert file

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/lambdaexpressions/notificationfiltering/Alert.java
@@ -0,0 +1,12 @@
+package com.lambdaexpressions.notificationfiltering;
+
+public class Alert {
+    String type;
+    String message;
+
+    Alert(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+}
+
