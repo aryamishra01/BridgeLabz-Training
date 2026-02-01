4d510a0ceba9397b1991102a4ff1f974f25638d4
aryamishra01 <aryagetgoals12@gmail.com>
2026-02-01 21:12:50 +0530

    [Arya Mishra] : added Product file

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/lambdaexpressions/customsorting/Product.java
@@ -0,0 +1,16 @@
+package com.lambdaexpressions.customsorting;
+
+public class Product {
+
+	String name;
+	double price;
+	double rating;
+	double discount;
+	
+	Product(String name, double price, double rating, double discount) {
+        this.name = name;
+        this.price = price;
+        this.rating = rating;
+        this.discount = discount;
+    }
+}
